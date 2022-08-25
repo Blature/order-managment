ea0528143879fe3aca862fc88306ddb903743d18
Ho3yn.Golchin <ho3yngolchin@gmail.com>
2022-08-25 09:56:40 +0430

    small changes

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const logger = new Logger('PortHandler');
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  logger.log(`We are online on port: ${port}`);
 }
 bootstrap();
